c3c5d69b9fd39fe68b633783497e905656c4b53b
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2023-03-17 11:56:30 +0300

    database layer
    base db layer

---
+++ backend/src/by/fpmibsu/find_a_friend/controller/Runner.java
@@ -0,0 +1,37 @@
+package by.fpmibsu.find_a_friend.controller;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class Runner {
+    public static void main(String[] args) {
+        var properties = new Properties();
+        try {
+            properties.load(new FileReader("config/config.properties"));
+        } catch (IOException exception) {
+            System.out.println("Failed to start the app, cannot read config");
+            exception.printStackTrace();
+            return;
+        }
+        var dbPath = (String) properties.getProperty("database_url");
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(dbPath);
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to the database.");
+            e.printStackTrace();
+            return;
+        }
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("Failed to closed db connection");
+            e.printStackTrace();
+            return;
+        }
+    }
+}
