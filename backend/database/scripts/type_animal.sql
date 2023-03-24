50837252f37d25cb94e44f092ac4495ce50012ae
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2023-03-24 19:36:08 +0300

    deleted file

--- backend/database/scripts/type_animal.sql
+++
@@ -1,7 +0,0 @@
-create table type_animal (
-	type_animal_id INT NOT NULL PRIMARY KEY,
-	name VARCHAR(50) NOT NULL
-);
-insert into type_animal (type_animal_id, name) values (1, 'кот');
-insert into type_animal (type_animal_id, name) values (2, 'собака');
-insert into type_animal (type_animal_id, name) values (3, 'лошадь');
