7e91e617abb6a7a7240d85f8926a1a763303a8ba
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2023-03-26 19:54:00 +0300

    added sql scripts and fixed entities
    This reverts commit 4202a9bfddcf03c2c7f37df48fe912b64393f20c.

--- backend/database-scripts/shelter.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE shelter(
-    shelter_id INT PRIMARY KEY AUTO INCREMENT,
-    place_id INT NOT NULL,
-    name VARCHAR(100) NOT NULL
-)
--- backend/database-scripts/type_person.sql
+++
@@ -1,6 +0,0 @@
-create table type_person (
-	type_person_id INT NOT NULL PRIMARY KEY,
-	name VARCHAR(50) NOT NULL
-);
-insert into type_person (type_person_id, name) values (1, 'ситтер');
-insert into type_person (type_person_id, name) values (2, 'волонтёр');
