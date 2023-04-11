874a524d7a26922cc94752bd48360bae330c2bf3
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2023-04-11 20:44:36 +0300

    added entity producer skeleton

---
+++ backend/src/main/java/by/fpmibsu/find_a_friend/data_access_layer/EntityProducer.java
@@ -0,0 +1,30 @@
+package by.fpmibsu.find_a_friend.data_access_layer;
+
+import by.fpmibsu.find_a_friend.entity.*;
+
+import java.sql.ResultSet;
+
+public class EntityProducer {
+    public static Advert makeAdvert(ResultSet set) {
+        return null;
+    }
+    public static AnimalAdvert makeAnimalAdvert(ResultSet set) {
+        return null;
+    }
+
+    public static Photo makePhoto(ResultSet set) {
+        return null;
+    }
+
+    public static Place makePlace(ResultSet set) {
+        return null;
+    }
+
+    public static Shelter makeShelter(ResultSet set) {
+        return null;
+    }
+
+    public static User makeUser(ResultSet set) {
+        return null;
+    }
+}
