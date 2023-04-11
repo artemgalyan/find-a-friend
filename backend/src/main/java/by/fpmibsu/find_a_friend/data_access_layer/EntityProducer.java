b707e4c4a3c3f28e0e1bffc535973b5ad83c7f3d
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2023-04-11 21:08:57 +0300

    implemented makePlace

--- backend/src/main/java/by/fpmibsu/find_a_friend/data_access_layer/EntityProducer.java
+++ backend/src/main/java/by/fpmibsu/find_a_friend/data_access_layer/EntityProducer.java
@@ -3,6 +3,7 @@ package by.fpmibsu.find_a_friend.data_access_layer;
 import by.fpmibsu.find_a_friend.entity.*;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class EntityProducer {
     public static Advert makeAdvert(ResultSet set) {
@@ -16,8 +17,14 @@ public class EntityProducer {
         return null;
     }
 
-    public static Place makePlace(ResultSet set) {
-        return null;
+    public static Place makePlace(ResultSet resultSet) throws SQLException {
+        Place place = new Place();
+        place.setId(resultSet.getInt("place_id"));
+        place.setCountry(resultSet.getString("country"));
+        place.setRegion(resultSet.getString("region"));
+        place.setCity(resultSet.getString("city"));
+        place.setDistrict(resultSet.getString("district"));
+        return place;
     }
 
     public static Shelter makeShelter(ResultSet set) {
