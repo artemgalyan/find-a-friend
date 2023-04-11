f1a51cda8f74054dc3c227930c1d3dc80cc1a497
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2023-04-11 22:16:56 +0300

    updated with default constructor

--- backend/src/main/java/by/fpmibsu/find_a_friend/entity/Photo.java
+++ backend/src/main/java/by/fpmibsu/find_a_friend/entity/Photo.java
@@ -3,6 +3,9 @@ package by.fpmibsu.find_a_friend.entity;
 public class Photo extends Entity {
     private byte[] data;
 
+    public Photo() {
+    }
+
     public Photo(byte[] data) {
         this.data = data;
     }
