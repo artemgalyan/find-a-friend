1ad5d7f762bfd8c3cad3a25c4e6a40a5ccea7830
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2023-05-02 18:12:37 +0300

    created parser

--- backend/src/main/java/by/fpmibsu/findafriend/controller/QueryParser.java
+++ backend/src/main/java/by/fpmibsu/findafriend/controller/QueryParser.java
@@ -5,7 +5,17 @@ import java.util.TreeMap;
 
 public class QueryParser {
     public Map<String, String> parseQuery(String query) {
-        // TODO: Implement this method
-        return new TreeMap<>();
+        if (query.indexOf('?')==-1){
+            return new TreeMap<String, String>();
+        }
+        Map<String, String> map = new TreeMap<>();
+        query = query.substring(query.indexOf('?')+1, query.length());
+        String[] params = query.split("&");
+        for (String param : params) {
+            String name = param.split("=")[0];
+            String value = param.split("=")[1];
+            map.put(name, value);
+        }
+        return map;
     }
 }
