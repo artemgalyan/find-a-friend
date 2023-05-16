b25418632727f1cbd9d78646962dc0c20711c2ca
Angelina-Varvashevich <90001649+Angelina-Varvashevich@users.noreply.github.com>
2023-05-16 16:28:51 +0300

    updated UserController

--- backend/src/main/java/by/fpmibsu/findafriend/controller/controllers/UserController.java
+++ backend/src/main/java/by/fpmibsu/findafriend/controller/controllers/UserController.java
@@ -3,6 +3,9 @@ package by.fpmibsu.findafriend.controller.controllers;
 import by.fpmibsu.findafriend.application.HandleResult;
 import by.fpmibsu.findafriend.application.controller.*;
 import by.fpmibsu.findafriend.application.mediatr.Mediatr;
+import by.fpmibsu.findafriend.controller.commands.users.CreateUserCommand;
+import by.fpmibsu.findafriend.controller.commands.users.DeleteUserCommand;
+import by.fpmibsu.findafriend.controller.commands.users.UpdateUserCommand;
 import by.fpmibsu.findafriend.controller.queries.users.GetUserByIdQuery;
 import by.fpmibsu.findafriend.controller.queries.users.GetUsersQuery;
 
@@ -23,5 +26,20 @@ public class UserController extends Controller {
     public HandleResult getById(@FromQuery(parameterName = "id") int id) {
 //        return new HandleResult(200, mediatr.send(new GetUserByIdQuery(id)));
         return Ok(mediatr.send(new GetUserByIdQuery(id)));
+    }
+
+    @Endpoint(path = "/createUser", method = HttpMethod.POST)
+    public HandleResult createPlace(@FromBody CreateUserCommand request) {
+        return Ok(mediatr.send(request));
+    }
+
+    @Endpoint(path = "/updateUser", method = HttpMethod.PUT)
+    public HandleResult updateUser(@FromBody UpdateUserCommand request) {
+        return Ok(mediatr.send(request));
+    }
+
+    @Endpoint(path = "/deleteUser", method = HttpMethod.DELETE)
+    public HandleResult deleteUserById(@FromQuery(parameterName = "id") int id) {
+        return Ok(mediatr.send(new DeleteUserCommand(id)));
     }
 }
