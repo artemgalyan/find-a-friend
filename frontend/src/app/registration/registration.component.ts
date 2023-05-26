72e7e45e766e68ac3993dc0c2267247d66a9c74b
artemgalyan <89996510+artemgalyan@users.noreply.github.com>
2023-05-26 20:55:21 +0300

    validation

--- frontend/src/app/registration/registration.component.ts
+++ frontend/src/app/registration/registration.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient, HttpErrorResponse, HttpResponse} from "@angular/common/http";
 import {Constants} from "../constants";
 import {Router} from "@angular/router";
@@ -8,7 +8,7 @@ import {Router} from "@angular/router";
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit {
 
   name: string = '';
   surname: string = '';
@@ -18,9 +18,56 @@ export class RegistrationComponent {
   phoneNumber: string = '';
 
   constructor(private httpClient: HttpClient,
-              private router: Router) {}
+              private router: Router) {
+  }
 
   onButtonClicked() {
+    let nameInput = document.querySelector('#nameInput') as HTMLInputElement
+    let surnameInput = document.querySelector('#surnameInput') as HTMLInputElement
+    let loginInput = document.querySelector('#loginInput') as HTMLInputElement
+    let passwordInput = document.querySelector('#passwordInput') as HTMLInputElement
+    let emailInput = document.querySelector('#emailInput') as HTMLInputElement
+    let phoneInput = document.querySelector('#phoneInput') as HTMLInputElement
+    let valid = true;
+    if (this.name.length === 0) {
+      valid = false;
+      nameInput.classList.add('is-invalid')
+    } else {
+      nameInput.classList.remove('is-invalid')
+    }
+    if (this.surname.length === 0) {
+      valid = false;
+      surnameInput.classList.add('is-invalid');
+    } else {
+      surnameInput.classList.remove('is-invalid')
+    }
+    if (this.login.length === 0) {
+      valid = false;
+      loginInput.classList.add('is-invalid');
+    } else {
+      loginInput.classList.remove('is-invalid')
+    }
+    if (this.password.length < 8) {
+      valid = false;
+      passwordInput.classList.add('is-invalid');
+    } else {
+      passwordInput.classList.remove('is-invalid')
+    }
+    if (this.email.length < 4) {
+      valid = false;
+      emailInput.classList.add('is-invalid');
+    } else {
+      emailInput.classList.remove('is-invalid')
+    }
+    if (this.phoneNumber.length < 7) {
+      valid = false;
+      phoneInput.classList.add('is-invalid');
+    } else {
+      phoneInput.classList.remove('is-invalid')
+    }
+    if (!valid) {
+      return
+    }
     this.httpClient.post<HttpResponse<any>>(Constants.api + '/users/createUser', {
       'name': this.name,
       'surname': this.surname,
@@ -34,10 +81,32 @@ export class RegistrationComponent {
       }
     }).subscribe(r => {
       this.router.navigate(['login'])
-    }, (e: HttpErrorResponse) =>{
+    }, (e: HttpErrorResponse) => {
       if (e.status == 400) {
         alert('Проверьте правильность введённой информации.');
       }
     })
+  }
+
+  ngOnInit(): void {
+    let nameInput = document.querySelector('#nameInput') as HTMLInputElement
+    let surnameInput = document.querySelector('#surnameInput') as HTMLInputElement
+    let loginInput = document.querySelector('#loginInput') as HTMLInputElement
+    let passwordInput = document.querySelector('#passwordInput') as HTMLInputElement
+    let emailInput = document.querySelector('#emailInput') as HTMLInputElement
+    let phoneInput = document.querySelector('#phoneInput') as HTMLInputElement
+    let validator = (element: HTMLInputElement, minSize: number = 1) => (e: Event) => {
+      if (element.value.length < minSize) {
+        element.classList.add('is-invalid')
+      } else {
+        element.classList.remove('is-invalid')
+      }
+    }
+    nameInput.oninput = validator(nameInput)
+    surnameInput.oninput = validator(surnameInput)
+    loginInput.oninput = validator(loginInput)
+    passwordInput.oninput = validator(passwordInput, 8)
+    emailInput.oninput = validator(emailInput, 3)
+    phoneInput.oninput = validator(phoneInput, 7)
   }
 }
